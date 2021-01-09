1ceb06a257e2edb762c3494e2ecfafbeb9ca3c97
Denis <den-282008@yandex.ru>
2021-01-09 14:44:08 +0300

    my commit

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Message from "./Message";
 
+type messageDataPropsType = {}
 const messageData = {
     avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
-    name: "Some Name",
-    message: "some text",
+    name: "Artem",
+    message: "npm start нажал?",
     time: "22:00",
 };
 
@@ -11,21 +13,8 @@ function HW1() {
     return (
         <div>
             <hr/>
-            homeworks 1
-
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
-            <hr/>
+            <Message avatar={messageData.avatar} name={messageData.name} message={messageData.message}
+                     time={messageData.time}/>
         </div>
     );
 }
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import a from "./Message.module.css"
 
-function Message() {
+type MessageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessageDataType) {
     return (
-        <div>
-
+        <div className={a.message}>
+            <div>
+                <img src={props.avatar} className={a.avatar}/>
+            </div>
+            <div className={a.content}>
+                <div className={a.name}>
+                    {props.name}
+                </div>
+                <div className={a.messageTime}>
+                    <div className={a.messageItem}>
+                        {props.message}
+                    </div>
+                    <div className={a.time}>
+                        {props.time}
+                    </div>
+                </div>
+            </div>
+            <div className={a.arrow}></div>
         </div>
     );
 }
