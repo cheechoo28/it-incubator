2b38baef18687027611d778592aae922bdcc7465
Denis <den-282008@yandex.ru>
2021-02-14 16:18:27 +0300

    Updates

---
+++ precache-manifest.b909cc2f0efbfc67d66ad4f3c493fd41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3872a4667e947430a954d568069c08c",
+    "url": "/it-incubator/index.html"
+  },
+  {
+    "revision": "56051e3a0a44a2242b46",
+    "url": "/it-incubator/static/css/main.a8dbd138.chunk.css"
+  },
+  {
+    "revision": "e7f03665563453496ef6",
+    "url": "/it-incubator/static/js/2.a6915928.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it-incubator/static/js/2.a6915928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56051e3a0a44a2242b46",
+    "url": "/it-incubator/static/js/main.b74f423f.chunk.js"
+  },
+  {
+    "revision": "f2110461bb488d4e5e5a",
+    "url": "/it-incubator/static/js/runtime-main.c07ca604.js"
+  }
+]);
