5d78381169fae19d51a4bf71367b1b9ea0e47080
Denis <den-282008@yandex.ru>
2021-03-30 17:19:37 +0300

    HW9, HW10  done

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import {RootStateOrAny, useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
 
 function HW10() {
     // useSelector, useDispatch
-    const loading = false;
+    const dispatch = useDispatch()
+    const loading = useSelector<AppStoreType, boolean>(state => state.loading.loading);
 
     const setLoading = () => {
         // dispatch
         // setTimeout
+        dispatch(loadingAC(true))
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 2000)
         console.log("loading...");
     };
+    console.log(loading)
 
     return (
         <div>
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,20 @@
 const initState = {
-
+    loading: false
 };
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionsType): any => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
-        }
+        case "SET-LOADING":
+            return {
+                ...state,
+                loading: action.value
+            }
         default: return state;
     }
 };
 
-export const loadingAC = (): any => {}; // fix any
+export const loadingAC = (value: boolean) => ({type: 'SET-LOADING', value} as const); // fix any
+
+type ActionsType = LoadingActionType
+
+type LoadingActionType = ReturnType<typeof loadingAC>
